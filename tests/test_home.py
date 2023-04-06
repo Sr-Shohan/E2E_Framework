2b00dbd40caa1cf091536e3a301e3ec51ee38959
Shohanur Rahman <shohanur@eskimi.com>
2023-04-07 00:34:38 +0600

    again test jenkins

--- tests/test_home.py
+++ tests/test_home.py
@@ -9,6 +9,6 @@ def test_google_search(driver):
     login_page.load()
     # Provide login information
     login_page.provide_login_info("standard_user", "secret_sauce")
-    # Verify that the user has successfully logged in
+    # Verify that the user has successfully logged
     assert "https://www.saucedemo.com/inventory.html" in driver.current_url
     time.sleep(3)
